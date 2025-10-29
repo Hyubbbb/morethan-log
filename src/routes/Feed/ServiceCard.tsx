16188cd737579ac91db6b618cad42c516d4e16fd
Hyubbbb <hyub@fnfcorp.com>
2025-10-29 17:55:51 +0900

    fix: 2차 수정 버그 3

--- src/routes/Feed/ServiceCard.tsx
+++ src/routes/Feed/ServiceCard.tsx
@@ -4,15 +4,21 @@ import { AiFillCodeSandboxCircle } from "react-icons/ai"
 import styled from "@emotion/styled"
 import { Emoji } from "src/components/Emoji"
 
+type Project = {
+  name: string
+  href: string
+}
+
 const ServiceCard: React.FC = () => {
-  if (!CONFIG.projects || CONFIG.projects.length === 0) return null
+  const projects = (CONFIG.projects || []) as Project[]
+  if (projects.length === 0) return null
   return (
     <>
       <StyledTitle>
         <Emoji>🌟</Emoji> Service
       </StyledTitle>
       <StyledWrapper>
-        {CONFIG.projects.map((project, idx) => (
+        {projects.map((project, idx) => (
           <a
             key={idx}
             href={`${project.href}`}
